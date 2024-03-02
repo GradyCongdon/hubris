15a13f8a8a0997a4a29c0452e03b7f1c6383dece
GradyCongdon <gradycongdon@gmail.com>
2024-03-01 19:18:57 -0500

    type fixes

--- app/rating/[name]/[rCode]/[characterShort]/page.tsx
+++ app/rating/[name]/[rCode]/[characterShort]/page.tsx
@@ -31,19 +31,25 @@ const example = {
 };
 
 const blocks = " ▁▂▃▄▅▆▇█".split("");
-function scaleConvert(number, inMin, inMax, outMin, outMax) {
+function scaleConvert(
+  number: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number
+) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
-// maps win percent to a block text based on the blocks array length
-const getClosestBlock = (value) => {
-  const converted = scaleConvert(value, 0, 100, 0, blocks.length - 1);
-  const index = Math.floor(converted);
-  console.log(index, converted, value, blocks[index]);
-  return blocks[index];
-};
+// // maps win percent to a block text based on the blocks array length
+// const getClosestBlock = (value) => {
+//   const converted = scaleConvert(value, 0, 100, 0, blocks.length - 1);
+//   const index = Math.floor(converted);
+//   console.log(index, converted, value, blocks[index]);
+//   return blocks[index];
+// };
 
 // postive is blue and negative is red scaled from -20 to 20
-const rgbScale = (value, min, max, opacity = 1) => {
+const rgbScale = (value: number, min: number, max: number, opacity = 1) => {
   // if (Math.abs(value) < 1) return "rgba(255, 255, 255, 0)";
   const range = max - min;
   const scaled = (value - min) / range;
@@ -76,7 +82,7 @@ export default async function Page({
   const rating = sets[0].Rating;
   const error = sets[0].Error;
   const gameCount = sets.reduce(
-    (acc, s) => {
+    (acc: { wins: any; losses: any }, s: { Wins: any; Losses: any }) => {
       acc.wins += s.Wins;
       acc.losses += s.Losses;
       return acc;
@@ -110,7 +116,7 @@ export default async function Page({
           maxHeight: maxHeight + "px"
         }}
       >
-        {sets.map((s) => {
+        {sets.map((s: any) => {
           const wins = s.Wins;
           const losses = s.Losses;
           const total = wins + losses;
@@ -146,7 +152,7 @@ export default async function Page({
       </div>
 
       <section className="">
-        {sets.map((s) => {
+        {sets.map((s: any) => {
           // const pRating = `${g.Rating}±${g.Error}`;
           // const oRating = `${g.OpponentRating}±${g.OpponentError}`;
           const pRating = s.Rating;
