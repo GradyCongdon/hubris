7fe848acf0560fcfc9580799028dda3c6021b863
GradyCongdon <gradycongdon@gmail.com>
2024-03-23 18:00:03 -0400

    no example

--- app/consts.ts
+++ app/consts.ts
@@ -2,11 +2,11 @@ export const DEFAULT_THEME = "light";
 export const MATCH_LIMIT = Infinity;
 export const HECK_CODE = "2EC3DCA33129F30";
 
-// export const EXAMPLE = false;
+export const EXAMPLE = false;
 export const POLLING_INTERVAL = 60 * 1000;
 export const SESSION_TIMEFRAME = 0;
 
 // Testing
-export const EXAMPLE = true;
+// export const EXAMPLE = true;
 // export const POLLING_INTERVAL = 4 * 1000;
 // export const SESSION_TIMEFRAME = 0;
