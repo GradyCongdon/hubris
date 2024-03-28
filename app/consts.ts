aa9358920743365d57e3b8bbcb40abcdbb64466e
GradyCongdon <gradycongdon@gmail.com>
2024-03-28 19:17:25 -0400

    no testing

--- app/consts.ts
+++ app/consts.ts
@@ -4,11 +4,11 @@ export const HECK_CODE = "2EC3DCA33129F30";
 export const THEMES = ["light", "dark", "utah"];
 
 
-// export const EXAMPLE = false;
-// export const POLLING_INTERVAL = 60 * 1000;
-// export const SESSION_TIMEFRAME = 0;
+export const EXAMPLE = false;
+export const POLLING_INTERVAL = 60 * 1000;
+export const SESSION_TIMEFRAME = 0;
 
 // Testing
-export const EXAMPLE = true;
-export const POLLING_INTERVAL = 4 * 1000;
-export const SESSION_TIMEFRAME = 0;
+// export const EXAMPLE = true;
+// export const POLLING_INTERVAL = 4 * 1000;
+// export const SESSION_TIMEFRAME = 0;
